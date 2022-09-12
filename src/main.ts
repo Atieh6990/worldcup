7f26138f3dec7600b9b05011d9fd218ed29aad71
Atieh6990 <93129399+Atieh6990@users.noreply.github.com>
2022-09-12 11:49:17 +0430

    initial

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import router from './worldcup/router'
+import store from './worldcup/store'
 
 Vue.config.productionTip = false
 
